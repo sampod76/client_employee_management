6ad0756c3aee92449deea00085ac184d73876d45
sampod76 <sampodnath76@gmail.com>
2024-11-21 10:23:27 +0600

    add login admin

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,14 @@
-import { Button, Checkbox, Flex, Form, Input, Row, Typography } from 'antd';
-import { FieldValues } from 'react-hook-form';
-
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import {
+  Button,
+  Checkbox,
+  Flex,
+  Form,
+  Input,
+  Row,
+  Tooltip,
+  Typography,
+} from 'antd';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useLoginMutation } from '../redux/features/auth/authApi';
@@ -12,18 +19,16 @@ import { ErrorModal } from '../utils/modalHook';
 import { verifyToken } from '../utils/verifyToken';
 
 const Login = () => {
+  const [loginDetails, setLoginDetails] = useState({ email: '', password: '' });
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const [error, setError] = useState('');
   const [login, { isLoading }] = useLoginMutation();
+  const [form] = Form.useForm(); // Create a form instance
 
-  const onSubmit = async (data: FieldValues) => {
-    console.log(data);
-
+  const onSubmit = async (data: any) => {
     try {
       const res = await login({ ...data }).unwrap();
-      console.log('🚀 ~ onSubmit ~ res:', res);
-
       const user = verifyToken(res.accessToken) as TUser;
       dispatch(
         setUser({
@@ -36,10 +41,14 @@ const Login = () => {
       navigate(`/${user.role}/dashboard`);
       setError('');
     } catch (err: any) {
-      console.log('🚀 ~ onSubmit ~ err:', err);
       setError(err?.message);
       ErrorModal(err);
     }
+  };
+
+  const handleQuickLogin = (email: string, password: string) => {
+    setLoginDetails({ email, password }); // Update state
+    form.setFieldsValue({ email, password }); // Update form fields
   };
 
   return (
@@ -52,24 +61,22 @@ const Login = () => {
         }}
         justify="center"
         align="middle"
-        // style={{ height: "100vh" }}
         className="h-fit w-fit p-16"
       >
         <div className="px-8 py-5">
           <div className="mb-6">
-            <h1 className="font-sans">{'Welcome'}</h1>
-            <p className="font-sans">
-              {'Please sign in for better experience'}
-            </p>
+            <h1 className="font-sans">Welcome</h1>
+            <p className="font-sans">Please sign in for better experience</p>
           </div>
           <Form
+            form={form} // Bind the form instance
             name="normal_login"
             className="login-form"
-            initialValues={{ remember: true }}
+            initialValues={{ remember: true, ...loginDetails }}
             onFinish={onSubmit}
           >
             <Typography.Title level={5}>
-              <span className="font-sans">{'Email'}</span>{' '}
+              <span className="font-sans">Email</span>{' '}
             </Typography.Title>
             <Form.Item
               name="email"
@@ -78,10 +85,10 @@ const Login = () => {
               <Input
                 size="large"
                 prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder={'Enter your email'}
+                placeholder="Enter your email"
               />
             </Form.Item>
-            <Typography.Title level={5}>{'Password'}</Typography.Title>
+            <Typography.Title level={5}>Password</Typography.Title>
             <Form.Item
               name="password"
               rules={[
@@ -91,20 +98,19 @@ const Login = () => {
               <Input.Password
                 size="large"
                 prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder={'Enter your password'}
+                placeholder="Enter your password"
               />
             </Form.Item>
             <Form.Item>
               <Flex justify="space-between">
                 <Form.Item name="remember" valuePropName="checked" noStyle>
                   <Checkbox>
-                    <span className="font-sans">{'Remember me'}</span>
+                    <span className="font-sans">Remember me</span>
                   </Checkbox>
                 </Form.Item>
 
                 <Link className="login-form-forgot" to={`/forgot-password`}>
-                  <span className="font-sans">{'Forgot password'}</span>
+                  <span className="font-sans">Forgot password</span>
                 </Link>
               </Flex>
             </Form.Item>
@@ -116,30 +122,37 @@ const Login = () => {
                 htmlType="submit"
                 className="login-form-button w-full"
               >
-                {'Login'}
+                Login
               </Button>
               <div className="mt-2 flex justify-end text-lg">
                 <Link className="login-form-forgot" to={`/register`}>
                   <span className="text-end font-sans">
-                    {'New Account Register'}
+                    New Account Register
                   </span>
                 </Link>
               </div>
               <p className="text-center text-red-500">{error}</p>
             </Form.Item>
           </Form>
-          <div className="rounded-lg border-2 border-blue-200 p-2">
-            <h1>
-              {' '}
-              Admin email : <span>admin@gmail.com</span> <br />
-              Password: 112233
-            </h1>
-            <br />
-            <h1>
-              {' '}
-              Employee email : <span>lenis95247@exweme.com</span> <br />
-              Password: 112233
-            </h1>
+          <div className="2 flex items-center justify-center gap-2 rounded-lg p-2">
+            <button
+              onClick={() => handleQuickLogin('admin@gmail.com', '112233')}
+              className="rounded-2xl border bg-white px-3 py-2"
+            >
+              <Tooltip title="Click then automatically login admin">
+                <span>Admin login</span>
+              </Tooltip>
+            </button>
+            <button
+              onClick={() =>
+                handleQuickLogin('lenis95247@exweme.com', '112233')
+              }
+              className="rounded-2xl border bg-white px-3 py-2"
+            >
+              <Tooltip title="Click then automatically login employee">
+                <span>Employee login</span>
+              </Tooltip>
+            </button>
           </div>
         </div>
       </Row>
